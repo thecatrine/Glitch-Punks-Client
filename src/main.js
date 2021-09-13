b9d23cb1afe99f67a75841bb8d765e92c0ce2c1a
Catherine David <miscatulated@gmail.com>
2021-09-13 15:05:59 -0500

    working sorta

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import * as bootstrap from 'bootstrap';
 
 import $ from 'jquery';
+import BN from "bn.js";
+
+import * as spl_token from "@solana/spl-token";
 
 let web3 = solanaWeb3
+
+let NFT_PROGRAM_ID = new web3.PublicKey("4x1tR9EdoduSpJsA6ZZYXprSE9VVd47EW9Sby9dq9qFy");
 
 async function connectButton() {
     const getProvider = async () => {
@@ -20,9 +25,35 @@ async function connectButton() {
     var provider = await getProvider();
     provider.connect();
 
-    provider.on("connect", () => {
+    provider.on("connect", async () => {
         console.log("connected!");
         console.log("Creator's public key: ", provider.publicKey.toString());
+
+        let initializerAccount = window.solana.publicKey;
+
+        const mintNFTTx = new web3.TransactionInstruction({
+            programId: NFT_PROGRAM_ID,
+            keys: [
+                { pubkey: initializerAccount, isSigner: true, isWritable: false },
+                { pubkey: NFT_PROGRAM_ID, isSigner: false, isWritable: true },
+                { pubkey: spl_token.TOKEN_PROGRAM_ID, isSigner: false, isWritable: false },
+            ],
+            data: Buffer.from(Uint8Array.of([new BN(1).toArray("le", 0)])),
+            //data: Buffer.from(Uint8Array.of(0, ...new BN(expectedAmount).toArray("le", 8)))
+        });
+
+        const tx = new web3.Transaction()
+            .add(mintNFTTx);
+
+        tx.recentBlockhash = (await connection.getRecentBlockhash('singleGossip')).blockhash;
+        tx.feePayer = initializerAccount;
+
+        const signedTransaction = await window.solana.signTransaction(tx);
+        const signature = await connection.sendRawTransaction(
+            signedTransaction.serialize(),
+            { skipPreflight: false, preflightCommitment: 'singleGossip' },
+        );
+
     })
 
     const connection = new web3.Connection(
